35d983bec0d88bd49e1783acdf814d1274ad7f59
scintilist <kylesmith2@gmail.com>
2015-01-24 21:23:29 -0500

    added dark buttons

---
+++ src/lib/button.py
@@ -0,0 +1,43 @@
+import pyglet
+
+from .game_obj import GameObj
+from . import util
+from .public_record import PublicRecord
+
+class ExitButton(GameObj):
+	def __init__(self, image = None, group = None, x = 0, y = 0, scale = 1.0, rotation = 0, 
+			visible = True, opacity = 255, record = PublicRecord(), room = None):
+		# Get the image here	
+		image = pyglet.resource.image("HubScreenExitBut_Dark.png")
+		super().__init__(image, group, x, y, scale, rotation, visible, opacity, record, room)
+		self.visible = False
+		
+	# EXCEPTIONS CAUGHT SILENTLY HERE BY PYGLET
+	def mouse_click(self, x, y):
+		self.visible = True
+
+	# EXCEPTIONS CAUGHT SILENTLY HERE BY PYGLET
+	def mouse_release(self, x, y):
+		self.visible = False
+		if self.room.is_over_object(self, x, y):
+			pyglet.app.exit()
+
+
+class PlayButton(GameObj):
+	def __init__(self, image = None, group = None, x = 0, y = 0, scale = 1.0, rotation = 0, 
+			visible = True, opacity = 255, record = PublicRecord(), room = None):
+		# Get the image here
+		image = pyglet.resource.image("HubScreenPlayBut_Dark.png")
+		image.anchor_x = image.width/2
+		super().__init__(image, group, x, y, scale, rotation, visible, opacity, record, room)
+		self.visible = False
+		
+	# EXCEPTIONS CAUGHT SILENTLY HERE BY PYGLET
+	def mouse_click(self, x, y):
+		self.visible = True
+
+	# EXCEPTIONS CAUGHT SILENTLY HERE BY PYGLET
+	def mouse_release(self, x, y):
+		self.visible = False
+		if self.room.is_over_object(self, x, y):
+			self.room.room_changer.change_room("placeholder")
