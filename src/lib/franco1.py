84a38994115c237ebf04d7943ffe1bb7214fda94
bitbanger <lxl5734@rit.edu>
2015-01-25 02:38:15 -0500

    doubledict

---
+++ src/lib/franco1.py
@@ -0,0 +1,17 @@
+N1 = "Oh...hey."
+N2 = "That's okay; I wouldn't expect anybody to recognize little ol' me anymore."
+
+K1 = "Who are you?"
+K2 = "James Franco!?! What are you doing on the moon?"
+K3 = "So, Franco - we meet again."
+
+franco1 = {
+	N1: {
+		K1: (N2, None),
+		K2: (N2, None),
+		K3: (N2, None),
+	},
+
+	N2: {
+	}
+}
